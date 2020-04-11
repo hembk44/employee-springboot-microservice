efd4739deca0a670f3589b2d5ab48e8715e09474
Hem BK <baralhems12@gmail.com>
2020-04-11 17:30:41 -0500

    server-app folder and files deleted

--- server-app/src/main/java/com/imcs/employeeServiceApp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imcs.employeeServiceApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.imcs.employeeServiceApp.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
